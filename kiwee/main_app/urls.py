7cdf74f68d01fad0be7ceb92dbfa95db66211bbb
ddanieela1 <vdaniela.gv@gmail.com>
2022-12-10 17:33:05 -0500

    feat logout added

--- kiwee/main_app/urls.py
+++ kiwee/main_app/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('signup/', views.signup, name ='signup'),
     path('login/', views.login_view, name ='login'),
     path('user/<username>/', views.profile, name='profile'),
+     path('logout/', views.logout_view, name='logout'),
 ]
