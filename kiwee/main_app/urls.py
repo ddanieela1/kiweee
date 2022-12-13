28c50766948e128b4bf6e1d8a2de7b1a165f60c5
ddanieela1 <vdaniela.gv@gmail.com>
2022-12-12 23:32:14 -0500

    updated sign up view, added settings url/view

--- kiwee/main_app/urls.py
+++ kiwee/main_app/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('login/', views.login_view, name ='login'),
     path('user/<username>/', views.profile, name='profile'),
     path('logout/', views.logout_view, name='logout'),
+    path('settings/', views.settings, name='settings'),
  
 ]
--- kiwee/main_app/views.py
+++ kiwee/main_app/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.http import HttpResponseRedirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.core.exceptions import ValidationError
 from django.contrib.auth.models import User
+
 
 # Create your views here.
 def index(request):
@@ -17,6 +20,7 @@ def homepage(request):
 def profile(request, username):
   user = User.objects.get(username=username)
   return render(request, 'profile.html', { 'username': username})
+
 
 def login_view(request):
     if request.method == 'POST':
@@ -44,14 +48,51 @@ def logout_view(request):
 def signup(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
+
         if form.is_valid():
             u = form.cleaned_data['username']
             user = form.save()
             login(request, user)
             return HttpResponseRedirect('/user/'+u)
+
+        elif request.POST['username']==request.POST['username'].exists():
+            username = form.cleaned_data['username']
+            messages.info(request, 'Username already in use.')
+            return redirect('/signup')
+
         else:
           return render(request, 'signup.html', {'form': form })
+
     else:
         form = UserCreationForm()
         return render(request, 'signup.html', {'form': form })
+
+@login_required
+def settings(request):
+    user_profile = UserCreationForm(request.POST)
+
+    if request.method == 'POST':
+
+        if request.FILES.get('image') != None:
+            image = request.FILES.get('profile_image')
+            about = request.POST['about']
+            location = request.POST['location']
+
+            user_profile.profile_image = image
+            user_profile.about = about
+            user_profile.location = location
+            user_profile.save()
+
+        elif request.FILES.get('image') == None: 
+            image = user_profile.profile_image
+            about = request.POST['about']
+            location = request.POST['location']
+
+            user_profile.profile_image = image
+            user_profile.about = about
+            user_profile.location = location
+            user_profile.save()
+
+        return redirect('settings')
+    return render (request, 'settings.html', {'user_profile': user_profile})        
 
