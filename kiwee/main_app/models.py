4d0fd4318ed5f4ea8312eedf6e7c9884e90bd2fd
ddanieela1 <vdaniela.gv@gmail.com>
2022-12-15 17:24:29 -0500

    feat category model, updated profile and post model

--- kiwee/main_app/models.py
+++ kiwee/main_app/models.py
@@ -9,7 +9,6 @@ from datetime import datetime
 class Profile(models.Model):
     name = models.CharField(max_length=50)
     profile_image = models.ImageField('profile_imgs', default= 'media/kiwi-default-pic.jpeg')
-    likes = models.IntegerField()
     about = models.TextField(max_length= 1000, blank = True)
     location = models.TextField(blank = True, max_length= 100)
     user = models.ForeignKey(User,on_delete=models.CASCADE)
@@ -19,6 +18,12 @@ class Profile(models.Model):
     def __str__(self):
         return self.user.username
 
+class Category(models.Model):
+    name = models.CharField(max_length=100, null = False, blank = False)
+
+    def __str__(self):
+        return self.name
+
 class Post(models.Model):
     # unique id for post
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
@@ -26,7 +31,10 @@ class Post(models.Model):
     media = models.ImageField(upload_to='posted_media')
     caption = models.TextField()
     created_at = models.DateTimeField(default=datetime.now)
-    likes = models.IntegerField(default=0)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
+
 
     def __str__(self):
         return self.user
+
+
